ad51d28064286f64c2ccc834f8f77d1776404de4
waynegeezer <wayne17gonsalves@gmail.com>
2021-10-02 17:44:38 +0530

    soc additional information page

---
+++ additional-information.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="title" content="">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <title>Montance | Additional Information</title>
+    <link rel="canonical" href="" />
+    <link rel="icon" href="content/images/favicon.png">
+    <link rel="stylesheet" href="content/css/aos.css">
+    <link rel="stylesheet" href="content/css/bootstrap.min.css">
+    <link rel="stylesheet" href="content/css/styles.css">
+</head>
+<body class="soc-additional-information">
+    <?php
+        include ("content/views/header.php");
+    ?>
+    <main>
+        <section class="section-banner section-banner-illustration">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h1 class="dark-theme-color-white" data-aos="fade-up" data-aos-delay="200">References. Background. Online Class</span></h1>
+                        <p class="dark-theme-color-grey" data-aos="fade-up" data-aos-delay="200"> An online version of the class is not yet available, but is under development with Applied Network Defense. The delay in delivery of the course is resulting from Christopher Crowley's inclination toward classroom delivery, and AND's experience with online delivery modalities. More work is being done to produce the best quality course. If you want to discuss an interim remote delivery of the live class, please contact Christopher Crowley.</p>
+                        <img class="illustration-img" src="content/images/illustrations/learn-built.png" alt="" data-aos="fade-up" data-aos-delay="300">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div class="arrow-block d-sm-none">
+            <img src="content/images/icons/arrow-right.svg" alt="">
+        </div>
+        <section class="section-two-column">
+            <div class="container">
+                <div class="row row-two-column">
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
+                        <div class="js-tilt data-tilt"><img class="icon-img" src="content/images/icons/design-development-execution.png" alt=""></div>
+                    </div>
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+                        <p class="sub-sm-header">May, 2020, Webcast</p>
+                        <h2 class="dark-theme-color-white">Mobile Attack Surface & Assessments</h2>
+                        <p class="dark-theme-color-grey">Webcast on the entire mobile attack surface and frameworks for guiding assessments of vulnerabilities and risk.</p>
+                        <p class="dark-theme-color-grey">This talk was intended to provide an overview of the risk associated with mobile devices. It discusses consensus frameworks to guide security professionals on assessing the risk inherent in mobile device use.</p>
+                        <p class="dark-theme-color-grey"><a class="btn-text" href="https://www.sans.org/webcasts/mobile-assessments-attack-surface-frameworks-114985/" target="_blank">Watch the webcast</a> (requires SANS site registration)</p>
+                        <a class="btn btn-primary btn-primary-drive" href="https://drive.google.com/drive/folders/0B9l-G6EuipZgU2xxTU5yTlRsVVE?resourcekey=0-OxdlF_-Pcj3jPkzabC-4cQ" target="_blank"><img class="icon" src="content/images/icons/drive.svg" alt=""> Download the Slide Deck</a>
+                    </div>
+                </div>
+                <div class="row row-two-column">
+                    <div class="col-lg-6 order-lg-1" data-aos="fade-up" data-aos-delay="300">
+                        <div class="js-tilt data-tilt"><img class="icon-img" src="content/images/icons/implementation.png" alt=""></div>
+                    </div>
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+                        <p class="sub-sm-header">January, 2020 Austin, TX, USA</p>
+                        <h2 class="dark-theme-color-white">SOAR Brief</h2>
+                        <p class="dark-theme-color-grey">SANS Automation and Orchestration Solutions discussing automation and orchestration in Ausin, TX.</p>
+                        <p class="dark-theme-color-grey">Christopher Crowley was chairman at the SANS Automation and Orchestration Solutions Forum in January, 2020. He discussed automation and orchestration, analysis methodology, an extensive process list, and recommendations on what to automate and orchestrate. He provides guidance on selection criteria to prioritize automation and orchestration efforts.</p>
+                        <p class="dark-theme-color-grey"><a class="btn-text" href="https://www.sans.org/webcasts/112740?utm_medium=soc-class-pres" target="_blank">Watch the webcast</a> (requires SANS site registration)</p>
+                        <a class="btn btn-primary" href="https://drive.google.com/file/d/1BiW5xM3_uZ2kITqs8QY3OJzJtoCR2VDA/view"  target="_blank"><img class="icon" src="content/images/icons/drive.svg" alt="">Download Zip file</a>
+                    </div>
+                </div>
+                <div class="row row-two-column">
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="300">
+                        <div class="js-tilt data-tilt"><img class="icon-img" src="content/images/icons/implementation-consultation.png" alt=""></div>
+                    </div>
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+                        <p class="sub-sm-header">November, 2017, Boston, MA, USA</p>
+                        <h2 class="dark-theme-color-white">Use Case Development</h2>
+                        <p class="dark-theme-color-grey">Crowley's description of the program for creating use cases.</p>
+                        <p class="dark-theme-color-grey">We hear the term "Use Case" frequently. This is a term from UML, and was originally intended to depict the user's many and varied interactions with the system (under design). We create security use cases. These are usually intended to provide alerts which drive subsequent investigation and verification. This talk outlines a program for doing this.</p>
+                        <p class="dark-theme-color-grey"><a class="btn-text" href="https://www.sans.org/webcasts/security-operations-center-soc-briefing-104427/" target="_blank">Watch the webcast</a> (requires SANS site registration)</p>
+                        <a class="btn btn-primary btn-primary-drive" href="https://drive.google.com/file/d/1h5uwIo5r2xsrTwm8e_WHROdI9oXlsuM4/view" target="_blank"><img class="icon" src="content/images/icons/drive.svg" alt="">Download the slide deck</a>
+                    </div>
+                </div>
+                <div class="row row-two-column">
+                    <div class="col-lg-6 order-lg-1" data-aos="fade-up" data-aos-delay="300">
+                        <div class="js-tilt data-tilt"><img class="icon-img" src="content/images/icons/implementation.png" alt=""></div>
+                    </div>
+                    <div class="col-lg-6" data-aos="fade-up" data-aos-delay="200">
+                        <p class="sub-sm-header">July, 2017, New Orleans, LA, USA</p>
+                        <h2 class="dark-theme-color-white">Threat Hunting</h2>
+                        <p class="dark-theme-color-grey">Crowley's talk on how to leverage threat hunting in Security Operations.</p>
+                        <p class="dark-theme-color-grey"><a class="btn-text" href="https://www.youtube.com/watch?v=pDY639JsT7I&t=8s" target="_blank">Watch the video on youtube</a></p>
+                        <a class="btn btn-primary btn-primary-drive" href="https://drive.google.com/file/d/1QXiKwrHgJ1TcFWz75UITB8EzOIHU-xvb/view"  target="_blank"><img class="icon" src="content/images/icons/drive.svg" alt="">Download the slide deck</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php
+        include ("content/views/footer.php");
+    ?>
+
+    <script defer src="content/scripts/jquery.min.js"></script>
+    <script defer src="content/scripts/popper.min.js"></script>
+    <script defer src="content/scripts/bootstrap.min.js"></script>
+    <script defer src="content/scripts/aos.js"></script>
+    <script defer src="content/scripts/tilt.jquery.js"></script>
+    <script defer src="content/scripts/script.js"></script>
+</body>
+</html>
